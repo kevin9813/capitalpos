370e5f38aadeb33c7b0ae5d27fab657baf73e0c7
domus_desarrollo2 <desarrollo2@domus.la>
2025-12-05 11:27:11 -0500

    produc

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -98,5 +98,49 @@ class DatabaseSeeder extends Seeder
             RolePermission::create($rolePermission);
         }
 
+
+        $products = [
+            ['company_id' => 1, 'code' => '001', 'name', 'Bola negra', 'unit_type' => 'kl', 'price' => 38000],
+            ['company_id' => 1, 'code' => '002', 'name', 'Caderita', 'unit_type' => 'kl', 'price' => 34000],
+            ['company_id' => 1, 'code' => '003', 'name', 'Centro cadera', 'unit_type' => 'kl', 'price' => 30000],
+            ['company_id' => 1, 'code' => '004', 'name', 'Ampolleta especial', 'unit_type' => 'kl', 'price' => 28000],
+            ['company_id' => 1, 'code' => '005', 'name', 'Ampolleta corriente', 'unit_type' => 'kl', 'price' => 26000],
+            ['company_id' => 1, 'code' => '006', 'name', 'Muchacho', 'unit_type' => 'kl', 'price' => 22000],
+            ['company_id' => 1, 'code' => '007', 'name', 'Lomo redondo', 'unit_type' => 'kl', 'price' => 26000],
+            ['company_id' => 1, 'code' => '008', 'name', 'Lomo caracho', 'unit_type' => 'kl', 'price' => 32000],
+            ['company_id' => 1, 'code' => '009', 'name', 'Punta de anca', 'unit_type' => 'kl', 'price' => 32000],
+            ['company_id' => 1, 'code' => '010', 'name', 'Caderita super especial', 'unit_type' => 'kl', 'price' => 34000],
+            ['company_id' => 1, 'code' => '011', 'name', 'Lomo biche especial', 'unit_type' => 'kl', 'price' => 32000],
+            ['company_id' => 1, 'code' => '012', 'name', 'Lomo biche corriente', 'unit_type' => 'kl', 'price' => 30000],
+            ['company_id' => 1, 'code' => '013', 'name', 'Milanesa especial', 'unit_type' => 'kl', 'price' => 28000],
+            ['company_id' => 1, 'code' => '014', 'name', 'Pecho', 'unit_type' => 'kl', 'price' => 30000],
+            ['company_id' => 1, 'code' => '015', 'name', 'Sobrebarriga', 'unit_type' => 'kl', 'price' => 26000],
+            ['company_id' => 1, 'code' => '016', 'name', 'Espaldilla', 'unit_type' => 'kl', 'price' => 26000],
+            ['company_id' => 1, 'code' => '017', 'name', 'Sobaco crespa', 'unit_type' => 'kl', 'price' => 18000],
+            ['company_id' => 1, 'code' => '018', 'name', 'Pepino', 'unit_type' => 'kl', 'price' => 32000],
+            ['company_id' => 1, 'code' => '019', 'name', 'Molida especial', 'unit_type' => 'kl', 'price' => 28000],
+            ['company_id' => 1, 'code' => '020', 'name', 'Costi falda', 'unit_type' => 'kl', 'price' => 24000],
+            ['company_id' => 1, 'code' => '021', 'name', 'Morrillo', 'unit_type' => 'kl', 'price' => 25000],
+            ['company_id' => 1, 'code' => '022', 'name', 'Punta falda', 'unit_type' => 'kl', 'price' => 22000],
+            ['company_id' => 1, 'code' => '023', 'name', 'Costilla especail', 'unit_type' => 'kl', 'price' => 23000],
+            ['company_id' => 1, 'code' => '024', 'name', 'Costilla corriente', 'unit_type' => 'kl', 'price' => 20000],
+            ['company_id' => 1, 'code' => '025', 'name', 'Cogote especial', 'unit_type' => 'kl', 'price' => 19000],
+            ['company_id' => 1, 'code' => '026', 'name', 'Espinazo', 'unit_type' => 'kl', 'price' => 18000],
+            ['company_id' => 1, 'code' => '027', 'name', 'Llanera', 'unit_type' => 'kl', 'price' => 28000],
+            ['company_id' => 1, 'code' => '028', 'name', 'Coditos cerdo', 'unit_type' => 'kl', 'price' => 19000],
+            ['company_id' => 1, 'code' => '029', 'name', 'Hueso sustancia', 'unit_type' => 'kl', 'price' => 19000],
+            ['company_id' => 1, 'code' => '030', 'name', 'Paquete de hueso x3 libras', 'unit_type' => 'unidad', 'price' => 1000],
+            ['company_id' => 1, 'code' => '031', 'name', 'Cola res especial', 'unit_type' => 'kl', 'price' => 22000],
+            ['company_id' => 1, 'code' => '032', 'name', 'Aguja especial', 'unit_type' => 'kl', 'price' => 20000],
+            ['company_id' => 1, 'code' => '033', 'name', 'Higado nacional', 'unit_type' => 'kl', 'price' => 19000],
+            ['company_id' => 1, 'code' => '034', 'name', 'Boffe res', 'unit_type' => 'kl', 'price' => 16000],
+            ['company_id' => 1, 'code' => '035', 'name', 'Pajarilla', 'unit_type' => 'kl', 'price' => 17000],
+            ['company_id' => 1, 'code' => '036', 'name', 'Corazon', 'unit_type' => 'unidad', 'price' => 7500],
+            ['company_id' => 1, 'code' => '037', 'name', 'Callo', 'unit_type' => 'kl', 'price' => 24000],
+            ['company_id' => 1, 'code' => '038', 'name', 'Librillo callo', 'unit_type' => 'kl', 'price' => 19000],
+            ['company_id' => 1, 'code' => '039', 'name', 'Cuajo', 'unit_type' => 'kl', 'price' => 19000],
+            ['company_id' => 1, 'code' => '040', 'name', 'Sonrisa', 'unit_type' => 'kl', 'price' => 18000],
+        ];
+
     }
 }
